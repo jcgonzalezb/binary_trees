d1c2bdfd3bcb1f8ecd4c1e7e8fdf5bfd388448a8
alejandra hincapie <alejandra612@hotmail.com>
2021-11-30 14:58:49 -0800

    3-binary_tree_delete.c 3-main.c

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,19 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_delete - function that deletes
+ * an entire binary tree.
+ *
+ * @tree: tree
+ */
+void binary_tree_delete(binary_tree_t *tree)
+{
+	if (!tree)
+	{
+		return;
+	}
+
+	binary_tree_delete(tree->right);
+	binary_tree_delete(tree->left);
+	free(tree);
+}
