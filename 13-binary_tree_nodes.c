06318fdc535394f56d15419801d8edcc5d8548d7
jcgonzalezb <jcgonzalez@gmail.com>
2021-12-01 18:19:09 -0500

    First if fixed

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -3,7 +3,7 @@
 /**
  * binary_tree_nodes - function that that counts the nodes with
  * at least 1 child in a binary tree.
- * @tree: Pointer to the root node of the tree to traverse.
+ * @tree: Pointer to the tree node of the tree to traverse.
  * Return: If tree is NULL, the function must return 0.
  */
 size_t binary_tree_nodes(const binary_tree_t *tree)
@@ -11,9 +11,17 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 	if (!tree)
 		return (0);
 
-	if (tree->left == NULL && tree->right == NULL)
-		return (1);
+	if ((tree->left != NULL &&
+		tree->right == NULL) ||
+		(tree->left == NULL &&
+		tree->right != NULL) ||
+		(tree->left != NULL &&
+		tree->right != NULL))
+			return (1 + binary_tree_nodes(tree->left) +
+				binary_tree_nodes(tree->right));
 	else
-		return binary_tree_leaves(tree->left) +
-			binary_tree_leaves(tree->right);
+	{
+		return (binary_tree_nodes(tree->left) +
+		binary_tree_nodes(tree->right));
+	}
 }
