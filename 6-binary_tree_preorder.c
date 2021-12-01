0dd623e90a6ff3b8beb4f03ce6c8e491abab313d
jcgonzalezb <jcgonzalez@gmail.com>
2021-11-30 21:00:42 -0500

    Pending fixing number list

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -1,6 +1,5 @@
 #include "binary_trees.h"
 
-
 /**
  * binary_tree_preorder - function that goes through a binary tree using
  * pre-order traversal.
@@ -9,5 +8,10 @@
  */
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
-	
+	if (func == NULL || tree == NULL)
+		return;
+
+	binary_tree_preorder(tree, func);
+
+	binary_tree_preorder(tree, func);
 }
