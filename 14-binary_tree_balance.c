9c7aeb7a5d439f98f87a5227ac3215e3e32ac864
jcgonzalezb <jcgonzalez@gmail.com>
2021-12-01 20:58:39 -0500

    Main file for exercise 14

---
+++ 14-binary_tree_balance.c
@@ -0,0 +1,24 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_nodes - function that that counts the nodes with
+ * at least 1 child in a binary tree.
+ * @tree: Pointer to the tree node of the tree to traverse.
+ * Return: If tree is NULL, the function must return 0.
+ */
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+
+	if ((tree->left != NULL && tree->right == NULL) ||
+	    (tree->left == NULL && tree->right != NULL) ||
+	    (tree->left != NULL && tree->right != NULL))
+		return (1 + binary_tree_nodes(tree->left) +
+			binary_tree_nodes(tree->right));
+	else
+	{
+		return (binary_tree_nodes(tree->left) +
+		binary_tree_nodes(tree->right));
+	}
+}
